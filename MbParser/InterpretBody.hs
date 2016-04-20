7a84e49af197818ba6dcc09a44a44dbfa08cd2a6
Mikołaj Błaż <mb346862@students.mimuw.edu.pl>
2016-04-20 12:36:25 +0200

    process -> eval

--- MbParser/InterpretBody.hs
+++ MbParser/InterpretBody.hs
@@ -39,7 +39,7 @@ interpretDecls decls _ = interpretTmpVarDecls $ chooseVarDecls decls
 
 interpretTmpVarDecls :: [Decl] -> Err String
 interpretTmpVarDecls vds = do
-  finalState <- execStateT (processDecls vds) empty
+  finalState <- execStateT (evalDecls vds) empty
   return $ show finalState
 
 
@@ -48,12 +48,12 @@ type VarEnv = Map Var Integer
 type M = StateT VarEnv Err
 
 
-processDecls :: [Decl] -> M ()
-processDecls [] = return ()
-processDecls ((TmpVarDecl var e):ds) = do
-  n <- processExp e
+evalDecls :: [Decl] -> M ()
+evalDecls [] = return ()
+evalDecls ((TmpVarDecl var e):ds) = do
+  n <- evalExp e
   setStateValue var n
-  processDecls ds
+  evalDecls ds
 
 
 lookupVar name var_map = fromJust $ Data.Map.lookup name var_map
@@ -61,35 +61,35 @@ setValue var val var_map = Data.Map.insert var val var_map
 setStateValue var val = modify (setValue var val)
 
 
-processExp :: Exp -> M Integer
-processExp (Let ((TmpVarDecl var e1):_) e2) = do {
-	n1 <- processExp e1;
+evalExp :: Exp -> M Integer
+evalExp (Let ((TmpVarDecl var e1):_) e2) = do {
+	n1 <- evalExp e1;
   setStateValue var n1;
-	processExp e2;
+	evalExp e2;
 }
 
-processExp (If e1 e2 e3) = do {
-	n1 <- processExp e1;
-  processExp $ if n1 == 1 then e2 else e3;
+evalExp (If e1 e2 e3) = do {
+	n1 <- evalExp e1;
+  evalExp $ if n1 == 1 then e2 else e3;
 }
 
-processExp (OAdd e1 e2) = binOp e1 e2 (+)
-processExp (OSub e1 e2) = binOp e1 e2 (-)
-processExp (OMul e1 e2) = binOp e1 e2 (*)
-processExp (ODiv e1 e2) = do
-  n1 <- processExp e1
-  n2 <- processExp e2
+evalExp (OAdd e1 e2) = binOp e1 e2 (+)
+evalExp (OSub e1 e2) = binOp e1 e2 (-)
+evalExp (OMul e1 e2) = binOp e1 e2 (*)
+evalExp (ODiv e1 e2) = do
+  n1 <- evalExp e1
+  n2 <- evalExp e2
   if n2 /= 0
     then return $ n1 `div` n2
     else fail "Error: Division by 0"
 
-processExp (ONeg e1) = do
-  n1 <- processExp e1
+evalExp (ONeg e1) = do
+  n1 <- evalExp e1
   return (-n1)
 
-processExp (EOpE e1 compOp e2) = do {
-	n1 <- processExp e1;
-	n2 <- processExp e2;
+evalExp (EOpE e1 compOp e2) = do {
+	n1 <- evalExp e1;
+	n2 <- evalExp e2;
 	return $ if (n1 `evalOp` n2) then 1 else 0;
 } where evalOp = case compOp of {
   OEq  -> (==);
@@ -100,19 +100,19 @@ processExp (EOpE e1 compOp e2) = do {
   OGte -> (>=);
 }
 
-processExp (OAnd e1 e2) = binOp e1 e2 (*)
-processExp (OOr e1 e2) = binOp e1 e2 (\x y -> (x + y + 1) `div` 2)
+evalExp (OAnd e1 e2) = binOp e1 e2 (*)
+evalExp (OOr e1 e2) = binOp e1 e2 (\x y -> (x + y + 1) `div` 2)
 
-processExp (VarExp var) = gets $ lookupVar var
-processExp (LitExp (IntLit int)) = return int
+evalExp (VarExp var) = gets $ lookupVar var
+evalExp (LitExp (IntLit int)) = return int
 
-processExp (FApp e1 e2) = undefined
-processExp (GConExp gCon) = undefined
-processExp (TupleExp e1 es) = undefined
-processExp (ListExp es) = undefined
+evalExp (FApp e1 e2) = undefined
+evalExp (GConExp gCon) = undefined
+evalExp (TupleExp e1 es) = undefined
+evalExp (ListExp es) = undefined
 
 binOp :: Exp -> Exp -> (Integer -> Integer -> Integer) -> M Integer
 binOp e1 e2 op = do
-  n1 <- processExp e1
-  n2 <- processExp e2
+  n1 <- evalExp e1
+  n2 <- evalExp e2
   return $ n1 `op` n2
