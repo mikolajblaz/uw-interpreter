23984a9740c52e31cdb95411d42b44a1160f97e7
Mikołaj Błaż <mb346862@students.mimuw.edu.pl>
2016-04-22 14:47:57 +0200

    Local Env idea

--- MbParser/Environment.hs
+++ MbParser/Environment.hs
@@ -11,7 +11,9 @@ import ErrM
 --type DataEnv = Map Con DataDecl
 type DataEnv = String
 
-type Env = Map.Map Var Exp
+newtype Env = Env (Map.Map Var StaticExp)
+type StaticExp = (Exp, Env, LocalEnv)
+type LocalEnv = Map.Map Var Exp -- TODO: maybe (Exp, Env)
 
 -- | A monad to evaluate program in
 type M = ReaderT Env Err
@@ -19,8 +21,8 @@ type M = ReaderT Env Err
 -- | Operations on environment
 lookupVar :: Var -> Env -> Exp
 lookupVar var env = fromJust $ Map.lookup var env
-setVarValue :: Var -> Exp -> Env -> Env
-setVarValue var exp env = Map.insert var exp env
+
+setVar :: LocalEnv -> Var
 
 -- | Extract variable from declaration
 -- TODO: maybe extract many vars, if declaration is "Pattern = Exp"
@@ -35,14 +37,17 @@ getExp (Signature var ty) = undefined
 getExp (FunDecl _ _ _) = undefined
 getExp (TmpVarDecl _ exp) = exp
 
+
+
 -- | Turn list of declarations to a map, where each varibale has its own
 -- expression as value
 -- TODO: we assume, that each variable has one expression bound
 -- TODO: implements patterns
-collectLocalDecl :: [Decl] -> Map.Map Var Exp
+collectLocalDecl :: [Decl] -> LocalEnv
 collectLocalDecl decls = foldr insertDecl Map.empty decls
   where insertDecl d = Map.insert (getVar d) (getExp d)
 
 -- | Insert all declarations from local scope to bindings from outer scope.
 localToOuterEnv :: [Decl] -> Env -> Env
-localToOuterEnv localDecls env = Map.union (collectLocalDecl localDecls) env
+localToOuterEnv localDecls env = Map.foldr insertVar localEnv env
+  where localEnv = collectLocalDecl localDecls
