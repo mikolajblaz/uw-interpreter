84d9742cae288a279342d2feb3a307d082b3b864
Mikołaj Błaż <mb346862@students.mimuw.edu.pl>
2016-04-25 00:00:41 +0200

    Patterns module introduced

---
+++ MbParser/Patterns.hs
@@ -0,0 +1,32 @@
+module Patterns where
+
+import AbsMbCore
+import ErrM
+
+import Environment
+import Expressions
+
+data Exp
+    | VarExp Var
+    | GConExp GCon
+    | LitExp Literal
+    | TupleExp Exp [Exp]
+    | ListExp [Exp]
+  deriving (Eq, Ord, Show, Read)
+
+data Pat
+    = ManyGConPat GCon [Pat]
+    | VarPat Var
+    | ZeroGConPat GCon
+    | LitPat Literal
+    | WildCard
+    | TuplePat Pat [Pat]
+    | ListPat [Pat]
+  deriving (Eq, Ord, Show, Read)
+
+data GCon = SimpleCon Con | UnitCon | ListCon | TupleCon [Comma]
+  deriving (Eq, Ord, Show, Read)
+
+matchAgainst :: Pat -> Exp -> Env -> LocalEnv -> LocalEnv
+matchAgainst pat exp env = case (pat, exp) of
+  (VarPat var, e) -> setLocalVar var e
