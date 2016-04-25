f71e8109ef8a53f7469201ef296c79a5e174fe08
Mikołaj Błaż <mb346862@students.mimuw.edu.pl>
2016-04-25 17:41:42 +0200

    Comment patterns

--- MbParser/Patterns.hs
+++ MbParser/Patterns.hs
@@ -6,27 +6,30 @@ import ErrM
 import Environment
 import Expressions
 
-data Exp
-    | VarExp Var
-    | GConExp GCon
-    | LitExp Literal
-    | TupleExp Exp [Exp]
-    | ListExp [Exp]
-  deriving (Eq, Ord, Show, Read)
+-- data Exp
+--     | VarExp Var
+--     | GConExp GCon
+--     | LitExp Literal
+--     | TupleExp Exp [Exp]
+--     | ListExp [Exp]
+--   deriving (Eq, Ord, Show, Read)
+--
+-- data Pat
+--     = ManyGConPat GCon [Pat]
+--     | VarPat Var
+--     | ZeroGConPat GCon
+--     | LitPat Literal
+--     | WildCard
+--     | TuplePat Pat [Pat]
+--     | ListPat [Pat]
+--   deriving (Eq, Ord, Show, Read)
+--
+-- data GCon = SimpleCon Con | UnitCon | ListCon | TupleCon [Comma]
+--   deriving (Eq, Ord, Show, Read)
 
-data Pat
-    = ManyGConPat GCon [Pat]
-    | VarPat Var
-    | ZeroGConPat GCon
-    | LitPat Literal
-    | WildCard
-    | TuplePat Pat [Pat]
-    | ListPat [Pat]
-  deriving (Eq, Ord, Show, Read)
-
-data GCon = SimpleCon Con | UnitCon | ListCon | TupleCon [Comma]
-  deriving (Eq, Ord, Show, Read)
-
-matchAgainst :: Pat -> Exp -> Env -> LocalEnv -> LocalEnv
-matchAgainst pat exp env = case (pat, exp) of
-  (VarPat var, e) -> setLocalVar var e
+matchAgainst :: Pat -> Exp -> Env -> LocalEnv -> Err LocalEnv
+matchAgainst pat exp env = do
+  evaledExp <- runExp exp env
+  case (pat, evaledExp) of
+    (VarPat var, e) -> setLocalVar var e
+    (WildCard, e) -> Ok
